b90918c699f4f4d8be159bf3943f28eba1487bf5
Tom Mardis <falldeaf@gmail.com>
2020-09-14 15:11:38 -0400

    ux updates

--- index.js
+++ index.js
@@ -56,4 +56,4 @@ cron.schedule("30 */3 * * *", async () => {
 
 //tasks.followTask("#indiedev", 0.5, 5, 6);
 //tasks.unfollowTask();
-database.getFollowersForConfigs();
+//database.getFollowersForConfigs();
